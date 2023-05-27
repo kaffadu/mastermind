d7e0034a4d08550bc56a49fb03b5ec7688c28e54
Enoch Sasu Edwards <enoch.s.edwards@gmail.com>
2023-05-28 00:25:05 +0100

    added mastermind sub1

--- main.tf
+++ main.tf
@@ -11,3 +11,15 @@ resource "aws_vpc" "Mastermind-vpc" {
     Environment = "Prod"
   }
 }
+
+
+# Public subnets
+resource "aws_subnet" "Mastermind-sub1" {
+  vpc_id            = aws_vpc.Mastermind-vpc.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "Mastermind-sub1"
+  }
+}
