93db8be26b4b68cd16b1a0bb4737f8edaf63a648
John Osei-Ansah <johnoseiansah@gmail.com>
2023-05-28 00:34:47 +0100

    mastermind sub2

--- main.tf
+++ main.tf
@@ -11,3 +11,16 @@ resource "aws_vpc" "Mastermind-vpc" {
     Environment = "Prod"
   }
 }
+
+
+# mastermind subnet 2
+resource "aws_subnet" "Mastermind_sub2" {
+  vpc_id     = aws_vpc.Mastermind-vpc.id
+  cidr_block = "10.0.11.0/24"
+  availability_zone = "eu-west-1b"
+
+  tags = {
+    Name        = "Mastermind_sub2"
+    Environment = "Prod"
+  }
+}
