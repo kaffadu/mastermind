f2eaea1dff8d120f9a32095c54f0ddd9805b1288
John Osei-Ansah <johnoseiansah@gmail.com>
2023-05-28 00:55:07 +0100

    rt assoc

--- main.tf
+++ main.tf
@@ -15,12 +15,30 @@ resource "aws_vpc" "Mastermind-vpc" {
 
 # mastermind subnet 2
 resource "aws_subnet" "Mastermind_sub2" {
-  vpc_id     = aws_vpc.Mastermind-vpc.id
-  cidr_block = "10.0.11.0/24"
+  vpc_id            = aws_vpc.Mastermind-vpc.id
+  cidr_block        = "10.0.11.0/24"
   availability_zone = "eu-west-1b"
 
   tags = {
     Name        = "Mastermind_sub2"
     Environment = "Prod"
   }
+}
+
+
+# mastermind private rt
+resource "aws_route_table" "mastermind_prv_rt" {
+  vpc_id = aws_vpc.Mastermind-vpc.id
+
+  tags = {
+    Name        = "mastermind_prv_rt"
+    Environment = "Prod"
+  }
+}
+
+
+# mastermind route table association
+resource "aws_route_table_association" "private-rt-assoc" {
+  subnet_id      = aws_subnet.Mastermind_sub2.id
+  route_table_id = aws_route_table.mastermind_prv_rt.id
 }
