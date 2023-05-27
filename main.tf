6bb79c65da9f36bda48617c5d76acf67594940a4
Patrick Affadu <kaffadu@icloud.com>
2023-05-27 21:46:35 +0100

    Mastermind Test

---
+++ main.tf
@@ -0,0 +1,13 @@
+# Git Mastermind VPC
+resource "aws_vpc" "Mastermind-vpc" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+
+  tags = {
+    Name        = "Mastermind-vpc"
+    Environment = "Dev"
+  }
+}
