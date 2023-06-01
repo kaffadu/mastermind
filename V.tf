322077e1ff7a81c036c250cd5f3c0137e8eea4e7
Hetty <hettybema@gmail.com>
2023-06-01 22:26:43 +0100

    I have made changes

--- V.tf
+++ V.tf
@@ -1,2 +1,2 @@
 Lady Engineer V, "I'm loving this!" - "Teamwork makes the Dream Work" 
-Actually V, I agree with you. Yoiu owe me a cup of hot chocolate.
+Actually V, I agree with you. You owe me a cup of hot chocolate.
